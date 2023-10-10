00548b75709146be254a3a959ce105d734a40679
Angel Velasco <angelgg2020@outlook.com>
2023-10-10 12:29:06 -0500

    feat: :sparkles: Type alias in objects

--- introduction.ts
+++ introduction.ts
@@ -1,50 +1,74 @@
 //? Anotacion de tipo
 let name1: string = "Juan";
 
-
 //? Inferencia de tipo
 const person = {
-    name : 'juan',
-    age : 28
-}
+  name: "juan",
+  age: 28,
+};
 
 //? Eliminar Tipado
 let euler: any = 3.1785;
-euler = "euler"
+euler = "euler";
 
 //? Functions (En las funciones typescript no es capáz de inferir su tipo, por lo que debes definirlo)
 function sum(a: number, b: number) {
-    return a + b
+  return a + b;
 }
 sum(5, 4);
-sum("Hi", 2);
 
 //? Tipar objetos
-function sayHello({name, age}: {name: string, age: number}) {
-    return `Hi ${name}, you're ${age}`
+function sayHello({ name, age }: { name: string; age: number }) {
+  return `Hi ${name}, you're ${age}`;
 }
 
-sayHello({name: "Juan", age: 5})
+sayHello({ name: "Juan", age: 5 });
 
 //? Tipar valores que retorna (Return)
-function sayHi({name, age}: {name: string, age: number}): string {
-    console.log(`Hi ${name}, you're ${age}`);
-    return name;
+function sayHi({ name, age }: { name: string; age: number }): string {
+  console.log(`Hi ${name}, you're ${age}`);
+  return name;
 }
-console.log(sayHi({name: "Juan", age: 5})
-);
+console.log(sayHi({ name: "Juan", age: 5 }));
 
 //? Tipar funciones (Callbacks)
 //? Void indica que no le importa lo que devuelve una funcion
 const sayHiFromFun = (fn: (name: string, age: number) => void) => {
-    fn("Juan", 25)
+  fn("Juan", 25);
 };
 const sayHI = (name: string, age: number) => {
-    console.log(`Hi ${name}, you're ${age}`);
-}
+  console.log(`Hi ${name}, you're ${age}`);
+};
 sayHiFromFun(sayHI);
 
-
 //? Tipar arrow functions
+const sumNumbers = (a: number, b: number): number => {
+  return a + b;
+};
 
 //? Never nos permite decir que una funcion NUNCA devolvera nada
+function throwError(message: string): never {
+  throw new Error(message);
+}
+
+//? Inferencia en funciones
+const avengers = ["thor", "hulk", "ironman", "captain"];
+avengers.map((avenger) => {
+  console.log(avenger.toUpperCase());
+});
+
+//? Objetos (Type Alias)
+type Hero = {
+  name: string;
+  age: number;
+}
+let hero: Hero = {
+  name: "Flash",
+  age: 35
+}
+
+function createHero(hero: Hero): Hero {
+  const { name, age } = hero;
+  return { name, age }
+}
+const thor = createHero({name: "Thor", age: 25});
